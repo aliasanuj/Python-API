3fa18d62c09811471f96a0f3d69f36360c9c27b6
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-03-27 17:04:05 +0530

    Create API_Links.py

---
+++ Rest API/API_Links.py
@@ -0,0 +1,8 @@
+API implementation :
+==========================
+1. https://realpython.com/api-integration-in-python/
+2. https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
+3. https://www.dataquest.io/blog/python-api-tutorial/
+4. https://www.fullstackpython.com/api-creation.html
+5. https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
+6. https://www.pythonforbeginners.com/api/
